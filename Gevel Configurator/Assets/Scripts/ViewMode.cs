74924dad29c9828b98e1672c8fc0d6c1adbb15b6
Cor de Kruijf <cor.dekruijf@student.hu.nl>
2020-11-05 12:13:17 +0100

    Add present mode

---
+++ Gevel Configurator/Assets/Scripts/ViewMode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewMode : MonoBehaviour
+{
+    public GameObject EditMode;
+    public GameObject PresentMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PresentMode.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowEditMode()
+    {
+        EditMode.gameObject.SetActive(true);
+        PresentMode.gameObject.SetActive(false);
+    }
+
+    public void ShowPresentMode()
+    {
+        EditMode.gameObject.SetActive(false);
+        PresentMode.gameObject.SetActive(true);
+    }
+}
