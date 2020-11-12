fde68dd2393f9b902e173483291603289d3b1ed2
Cor de Kruijf <cor.dekruijf@student.hu.nl>
2020-11-12 11:07:00 +0100

    Fix object items position in scroll view

--- Gevel Configurator/Assets/Scripts/SceneObjects.cs
+++ Gevel Configurator/Assets/Scripts/SceneObjects.cs
@@ -7,32 +7,41 @@ public class SceneObjects : MonoBehaviour
 {
     public GameObject assetsView;
 
-    //List<GameObject> objectList = new List<GameObject>();
-
     // Start is called before the first frame update
     void Start()
     {
         int counter = 0;
-        int position = 0;
+        int position = -10;
 
         foreach (GameObject go in GameObject.FindObjectsOfType(typeof(GameObject)))
         {
             GameObject childObject = new GameObject("Object" + counter, typeof(RectTransform));
             childObject.transform.SetParent(this.assetsView.transform);
 
+            // Position of object item
             RectTransform rt = childObject.GetComponent<RectTransform>();
-            rt.sizeDelta = new Vector2(150, 50);
+            rt.sizeDelta = new Vector2(150, 20);
             rt.anchoredPosition = new Vector2(0, position);
 
+            rt.anchorMin = new Vector2(0.5f, 1);
+            rt.anchorMax = new Vector2(0.5f, 1);
+            rt.pivot = new Vector2(0.5f, 1);
+
+            // Text styling
             Text childText = childObject.AddComponent<Text>();
             childText.text = go.name;
             childText.color = Color.black;
             childText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
-
-            //Debug.Log(go);
+            
             counter++;
-            position += 20;
+            position += -20;
         }
+
+        // Calculate height for objects list
+        int assetsViewHeight = (20 * counter) + 20;
+
+        RectTransform AssetsViewRt = assetsView.GetComponent<RectTransform>();
+        AssetsViewRt.sizeDelta = new Vector2(0, assetsViewHeight);
     }
 
     // Update is called once per frame
