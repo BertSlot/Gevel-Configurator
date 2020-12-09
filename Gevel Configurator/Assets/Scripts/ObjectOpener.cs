474ce4290ed8351a075d42103f58d606a18ad55c
Nick Klomp <nick.klomp@student.hu.nl>
2020-12-09 16:07:19 +0100

    Added Assets/Startmenu functions

---
+++ Gevel Configurator/Assets/Scripts/ObjectOpener.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class ObjectOpener : MonoBehaviour
+{
+    string path;
+    public void OpenObject()
+    {
+        path = EditorUtility.OpenFilePanel("Overwrite with unity", "", "unity");
+        CheckObject();
+
+    }
+    void CheckObject()
+    {
+        if (path != null)
+        {
+            //UpdateText();
+            Debug.Log("Object geopend!");
+        }
+    }
+}
---
+++ Gevel Configurator/Assets/Scripts/ProjectOpener.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class ProjectOpener : MonoBehaviour
+{
+string path;
+    public void OpenProject()
+    {
+        path = EditorUtility.OpenFilePanel("Overwrite with unity", "", "unity");
+        GetText();
+
+    }
+    void GetText()
+    {
+        if (path != null)
+        {
+            UpdateText();
+        }
+    }
+    void UpdateText()
+    {
+        string filename = Path.GetFileName(path);
+        string[] x = filename.Split('.');
+        Text txtMy = GameObject.Find("StartMenu/LastProject2Button/Text").GetComponent<Text>();
+        txtMy.text = x[0];
+    }
+}
