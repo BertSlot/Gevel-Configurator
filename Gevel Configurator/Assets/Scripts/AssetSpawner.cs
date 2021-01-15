54af7b8a18f1a1c46b75dde59853445621478fba
Nick Klomp <nick.klomp@student.hu.nl>
2021-01-15 15:23:59 +0100

    Added Window buttons
    Minimize screen, maximize screen and close window

--- Gevel Configurator/Assets/Scripts/AssetSpawner.cs
+++ Gevel Configurator/Assets/Scripts/AssetSpawner.cs
@@ -7,87 +7,86 @@ using UnityEngine;
 public class AssetSpawner : MonoBehaviour {
 
 
-	/// <summary>
-	/// Array for GameObjects loaded from Import_Objects
-	/// </summary>
-	private Object[] ObjectsList;
+    /// <summary>
+    /// Array for GameObjects loaded from Import_Objects
+    /// </summary>
+    private Object[] ObjectsList;
 
-	/// <summary>
-	/// This object is a child of GameObject 'objectList' containing the list of child objects
-	/// </summary>
-	[SerializeField]
-	private GameObject ObjectListContent;
+    /// <summary>
+    /// This object is a child of GameObject 'objectList' containing the list of child objects
+    /// </summary>
+    [SerializeField]
+    private GameObject ObjectListContent;
 
-	/// <summary>
-	/// Path to Import_Objects Folder
-	/// </summary>
-	[SerializeField]
-	private string ObjectsFolderPath = "Import_Objects";
+    /// <summary>
+    /// Path to Import_Objects Folder
+    /// </summary>
+    [SerializeField]
+    private string ObjectsFolderPath = "Import_Objects";
 
-	/// <summary>
-	/// AssetPanel prefab
-	/// </summary>
-	[SerializeField]
-	private GameObject PanelPrefab;
-
-
+    /// <summary>
+    /// AssetPanel prefab
+    /// </summary>
+    [SerializeField]
+    private GameObject PanelPrefab;
 
 
-	/*bool mouseClicksStarted = false;
-	int mouseClicks = 0;
-	float mouseTimerLimit = .25f;
-*/
+    // Add Object to a dictionary with Pair<Name,AssetPanel> for quick searching
+    private void Awake()
+    {
+        int x = 40;
+        int y = -40;
+        if (ObjectsFolderPath != null)
+        {
+            // Load all objects from folder
+            ObjectsList = Resources.LoadAll(ObjectsFolderPath, typeof(GameObject));
+            GameObject AssetPanel;
 
-	// Add Object to a dictionary with Pair<Name,AssetPanel> for cquick searching
-	private void Awake() {
+            // Make a panel sprite for every object in the folder
+            foreach (GameObject obj in ObjectsList)
+            {
+                // Create Panel
+                AssetPanel = Instantiate(PanelPrefab, ObjectListContent.transform);
 
-		int x = 40;
-		int y = -40;
-		if (ObjectsFolderPath != null) {
-			ObjectsList = Resources.LoadAll(ObjectsFolderPath, typeof(GameObject));
-			GameObject AssetPanel;
+                // Add Asset to panel and name under panel
+                SelectAsset panelData = AssetPanel.GetComponent<SelectAsset>();
+                panelData.Asset = obj;
+                panelData.AssetNameText = obj.name;
 
-			foreach (GameObject obj in ObjectsList) {
-				// Create Panel
-				AssetPanel = Instantiate(PanelPrefab, ObjectListContent.transform);
+                // Set position of Panel
+                RectTransform rt = AssetPanel.GetComponent<RectTransform>();
+                rt.anchoredPosition = new Vector2(x, y);
 
-				// Add Asset to panel and name under panel
-				SelectAsset panelData = AssetPanel.GetComponent<SelectAsset>();
-				panelData.Asset = obj;
-				panelData.AssetNameText = obj.name;
+                // Set Preview Sprite of Panel
+                SetPanelSprite(AssetPanel, obj, Color.black);
 
-				// Set position of Panel
-				RectTransform rt = AssetPanel.GetComponent<RectTransform>();
-				rt.anchoredPosition = new Vector2(x, y);
+                // Increase Offset
+                x += 120;
 
-				// Set Preview Sprite of Panel
-				SetPanelSprite(AssetPanel, obj, Color.black);
+            }
+        }
+    }
 
-				// Increase Offset
-				x += 120;
-
-			}
-		}
-
-
-	}
-
-	private void SetPanelSprite(GameObject obj, GameObject asset, Color BackgroundColor) {
-		Image img = obj.GetComponent<Image>();
-		if (img != null) {
-			RuntimePreviewGenerator.BackgroundColor = BackgroundColor;
-			Texture2D texture = RuntimePreviewGenerator.GenerateModelPreview(asset.transform, 100, 100, true);
-			Rect rect = new Rect(0, 0, texture.width, texture.height);
-			Vector2 pivot = new Vector2(1, 1);
-			Sprite preview = Sprite.Create(texture, rect, pivot);
-			img.sprite = preview;
-		}
-	}
+    // Create sprites for the assests menu
+    private void SetPanelSprite(GameObject obj, GameObject asset, Color BackgroundColor)
+    {
+        Image img = obj.GetComponent<Image>();
+        if (img != null)
+        {
+            RuntimePreviewGenerator.BackgroundColor = BackgroundColor;
+            Texture2D texture = RuntimePreviewGenerator.GenerateModelPreview(asset.transform, 100, 100, true);
+            Rect rect = new Rect(0, 0, texture.width, texture.height);
+            Vector2 pivot = new Vector2(1, 1);
+            Sprite preview = Sprite.Create(texture, rect, pivot);
+            img.sprite = preview;
+        }
+    }
 
 
-	private void Update() {
+    private void Update()
+    {
 
-	}
+    }
 }
 
 /*
--- Gevel Configurator/Assets/Scripts/SelectAsset.cs
+++ Gevel Configurator/Assets/Scripts/SelectAsset.cs
@@ -4,49 +4,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectAsset : MonoBehaviour, IPointerDownHandler {
+public class SelectAsset : MonoBehaviour, IPointerDownHandler
+{
 
-	public GameObject Asset;
-	[SerializeField]
-	private Text assetNameText; //null reference exception
-	public string AssetNameText {
-		get { return assetNameText.text; }
-		set { assetNameText.text = value; }
-	}
+    public GameObject Asset;
+    [SerializeField]
 
-	private RTG.GizmoManager gizmoManager;
+    private Text assetNameText; //null reference exception
 
+    // Get assetName data
+    public string AssetNameText
+    {
+        get { return assetNameText.text; }
+        set { assetNameText.text = value; }
+    }
 
-	// Click data for Asset  Menu clicks
-	float clicked = 0;
-	float clicktime = 0;
-	readonly float clickdelay = 0.25f;
-
+    private RTG.GizmoManager gizmoManager;
 
 
-	// Start is called before the first frame update
-	void Start() {
-		assetNameText = GetComponentInChildren<Text>();
-		gizmoManager = GameObject.Find("RTGizmoManager").GetComponent<RTG.GizmoManager>();
-
-	}
-
-	public void OnPointerDown(PointerEventData eventData) {
-		clicked++;
-		if (clicked == 1)
-			clicktime = Time.time;
-
-		if (clicked > 1 && Time.time - clicktime < clickdelay) {
-			clicked = 0;
-			clicktime = 0;
-			GameObject.Find("AssetsMenu").SetActive(false);
-			gizmoManager.SpawnObjectMiddle(Asset);
+    // Click data for Asset  Menu clicks
+    float clicked = 0;
+    float clicktime = 0;
+    readonly float clickdelay = 0.25f;
 
 
 
-		} else if (clicked > 2 || Time.time - clicktime > 1)
-			clicked = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        assetNameText = GetComponentInChildren<Text>();
+        gizmoManager = GameObject.Find("RTGizmoManager").GetComponent<RTG.GizmoManager>();
+    }
 
-	}
+    // Checks for double click
+    // If double clicked spawn object from assetsmenu
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        clicked++;
+        if (clicked == 1)
+            clicktime = Time.time;
 
+        if (clicked > 1 && Time.time - clicktime < clickdelay)
+        {
+            clicked = 0;
+            clicktime = 0;
+            GameObject.Find("AssetsMenu").SetActive(false);
+            gizmoManager.SpawnObjectMiddle(Asset);
+
+        }
+        else if (clicked > 2 || Time.time - clicktime > 1)
+            clicked = 0;
+    }
 }
