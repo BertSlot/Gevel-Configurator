0b60b610b730ecf6265226db5686298e7501f414
KingPungy <scott.timmermans@gmail.com>
2021-01-19 14:40:40 +0100

    pictures for readme info

--- Gevel Configurator/Assets/Scripts/MiniAndMaxiWindow.cs
+++ Gevel Configurator/Assets/Scripts/MiniAndMaxiWindow.cs
@@ -3,21 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MiniAndMaxiWindow : MonoBehaviour
-{
-    public void MiniMizeScreen()
-    {
-        Screen.SetResolution(640, 480, false);
-        Debug.Log("Minimize screen");
-        GameObject.Find("Navbar/UIButtons/MinimizeButton").SetActive(false);
-        GameObject.Find("Navbar/UIButtons/MaximizeButton").SetActive(true);
-    }
+public class MiniAndMaxiWindow : MonoBehaviour {
+	public void MiniMizeScreen() {
+		Screen.SetResolution(640, 480, false);
+		Debug.Log("Minimize screen");
+		GameObject.Find("Navbar/UIButtons/MinimizeButton").SetActive(false);
+		GameObject.Find("Navbar/UIButtons/MaximizeButton").SetActive(true);
+	}
 
-    public void MaxiMizeScreen()
-    {
-        Screen.SetResolution(640, 480, true);
-        Debug.Log("Maximize screen");
-        GameObject.Find("Navbar/UIButtons/MaximizeButton").SetActive(false);
-        GameObject.Find("Navbar/UIButtons/MinimizeButton").SetActive(true);
-    }
+	public void MaxiMizeScreen() {
+		Screen.SetResolution(640, 480, true);
+		Debug.Log("Maximize screen");
+		GameObject.Find("Navbar/UIButtons/MaximizeButton").SetActive(false);
+		GameObject.Find("Navbar/UIButtons/MinimizeButton").SetActive(true);
+	}
 }
