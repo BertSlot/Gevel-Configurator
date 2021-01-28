a0fbfce8ec12c5e6ec7c675d02caaa6e5863d736
scott <scott.timmermans@gmail.com>
2021-01-28 12:54:05 +0100

    player settings edit and resize, fullscreen fixed

--- Gevel Configurator/Assets/Scripts/MiniAndMaxiWindow.cs
+++ Gevel Configurator/Assets/Scripts/MiniAndMaxiWindow.cs
@@ -4,15 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MiniAndMaxiWindow : MonoBehaviour {
+
+	private void Start() {
+
+		Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+	}
+
 	public void MiniMizeScreen() {
-		Screen.SetResolution(640, 480, false);
+		// standard resolutrion for minimizen
+		Screen.SetResolution(1280, 720, false);
 		Debug.Log("Minimize screen");
 		GameObject.Find("Navbar/UIButtons/MinimizeButton").SetActive(false);
 		GameObject.Find("Navbar/UIButtons/MaximizeButton").SetActive(true);
 	}
 
 	public void MaxiMizeScreen() {
-		Screen.SetResolution(640, 480, true);
+		// resizes to fullscreen and uses the fullscreen resolution, so native resolution
+		Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
 		Debug.Log("Maximize screen");
 		GameObject.Find("Navbar/UIButtons/MaximizeButton").SetActive(false);
 		GameObject.Find("Navbar/UIButtons/MinimizeButton").SetActive(true);
